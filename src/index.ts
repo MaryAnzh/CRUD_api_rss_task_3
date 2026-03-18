6630ac74e2294157b906d196fa414875014c7762
MaryAnzh <maryang@mail.ru>
2026-03-18 21:12:31 +0300

    feat: add .env, env.PORT in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import Fastify from "fastify";
 import { productsRoutes } from "~routes";
 
+import dotenv from "dotenv";
+dotenv.config();
+// if .env  work - 3000 (code get from .env, not fom env.example), if doesn't - 3001
+const port = Number(process.env.PORT) || 3001;
+
 const app = Fastify({
     logger: true
 });
@@ -17,6 +22,6 @@ if (process.env.NODE_ENV !== "production") {
 
 app.register(productsRoutes, { prefix: "/api/products" });
 
-app.listen({ port: 3000 }).then(() => {
-    console.log("Server running on port 3000");
+app.listen({ port }).then(() => {
+    console.log(`Server running on port ${port}`);
 });
